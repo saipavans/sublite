ffbf1114eeaef5f2e6f2ba62dfd00b170b32ee7d
saipavans <sp403250@dal.ca>
2017-10-24 21:26:18 -0300

    closed file pointer

--- loadsub.py
+++ loadsub.py
@@ -8,21 +8,27 @@ TIME_DELIMITER = "-->"
 class SubLoader():
 
     def __init__(self, subfile):
-        fin = open(subfile)
-        sub_title = ""
-        sub_times = []
-        subtitle_data = {}
-        for line in fin:
-            if line.find(TIME_DELIMITER)!= -1:
-                line_stripped = line.strip()
-                sub_times = line_stripped.split(TIME_DELIMITER)
-                print("subtimes",sub_times)
-                subtitle_data[(sub_times[0],sub_times[1])] = sub_title
-                sub_title = ""
-            else:
-                line_stripped = line.strip()
-                sub_title += line_stripped
-        self.subtitle_data = subtitle_data
+        try:
+            fin = open(subfile)
+            sub_title = ""
+            sub_times = []
+            subtitle_data = {}
+            for line in fin:
+                if line.find(TIME_DELIMITER)!= -1:
+                    line_stripped = line.strip()
+                    sub_times = line_stripped.split(TIME_DELIMITER)
+                    print("subtimes",sub_times)
+                    subtitle_data[(sub_times[0],sub_times[1])] = sub_title
+                    sub_title = ""
+                else:
+                    line_stripped = line.strip()
+                    sub_title += line_stripped
+            self.subtitle_data = subtitle_data
+        except Exception as e:
+            LOGGER.exception(e)
+            self.subtitle_data = None
+        finally:
+            fin.close()
 
 if __name__ == '__main__':
     home_dir = os.path.expanduser("~")
