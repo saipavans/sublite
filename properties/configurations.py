fff63e863233bb93b890077ad46b4d34b8644c1b
saipavans <sp403250@dal.ca>
2017-10-25 22:34:02 -0300

    added configurations

--- properties/configurations.py
+++ properties/configurations.py
@@ -3,3 +3,8 @@ class Configuration():
 
     PROJECT_HOME = "."
     LOG_FILE = PROJECT_HOME + "/logs/tmp/subright.log"
+    SUBTITLE_SERVER = "http://api.opensubtitles.org/xml-rpc"
+    USER_AGENT_OPEN_SUTBTITLE = ""
+    USER_NAME_OPEN_SUBTITLE = ""
+    PASSWORD_OPEN_SUBTITLE = ""
+    LOG_FILE = "./logs/tmp/subright.log"
